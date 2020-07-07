6b03dc67fe88a84879d7e5bc7ebca14133f635f5
Naoya Yamaguchi <708yamaguchi@gmail.com>
2020-07-08 04:10:56 +0900

    Use /audio topic as the sound input

--- scripts/create_dataset.py
+++ scripts/create_dataset.py
@@ -20,24 +20,32 @@
 #                      - classB -- 001.png
 #                               |- 002.png
 #                               |- ...
-# dataset -- n_class.txt
-#         |- train_images.txt  # necessary for chainer
-#         |- test_images.txt  # necessary for chainer
+#
+# dataset -- train_images.txt  # necessary for chainer
+#         |- test_images.txt   # necessary for chainer
 #         |- train_(class)000*.png
 #         |- ...
 #         |- test_(class)000*.png
 #         |- ...
-
+#
+# n_class.txt
+#
+# Total data number
+# train: (number of images per class) * (train:test rate)       * (augment number)
+# test : (number of images per class) * (1 - (train:test rate))
 
 import argparse
+from process_gray_image import img_jet
 import imgaug as ia
 import imgaug.augmenters as iaa
+from PIL import Image as Image_
 import numpy as np
 import os
 import os.path as osp
-from PIL import Image as Image_
+import random
 import rospkg
 import shutil
+
 
 # for data augmentation
 ia.seed(1)
@@ -59,18 +67,32 @@ rospack = rospkg.RosPack()
 # Split dataset into train data and test data. The rate is given by --rate.
 def split():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-r', '--rate', default='0.8')  # train:test = 0.8:0.2
+    parser.add_argument('-r', '--rate', default='0.8', type=float,
+                        help='train:test dataset rate (default 0.8:0.2)')
     parser.add_argument('-p', '--path', default=osp.join(rospack.get_path(
-        'sound_classification'), 'train_data'))
-    parser.add_argument('-a', '--augment', default='5')  # create (augment) images per 1 image
+        'sound_classification'), 'train_data'), help='path to train data')
+    parser.add_argument('-a', '--augment', default='5', type=int,
+                        help='create {augment} images per 1 image')
+    parser.add_argument('-m', '--model', type=str,
+                        choices=['nin', 'vgg16'], default='nin',
+                        help='Neural network model to use dataset')
+    parser.add_argument('-n', '--number', default='100', type=int,
+                        help='maximum number of images per class used to create dataset')
     args = parser.parse_args()
-    rate = float(args.rate)
+    rate = args.rate
+    if args.model == 'nin':
+        image_size = (227, 227)
+    elif args.model == 'vgg16':
+        image_size = (224, 224)
+    else:
+        print('Model type {} is invalid.'.format(args.model))
+        exit()
     root_dir = osp.expanduser(args.path)
     origin_dir = osp.join(root_dir, 'original_spectrogram')
     dataset_dir = osp.join(root_dir, 'dataset')
     image_list_train = []
     image_list_test = []
-    mean_of_dataset = np.zeros((256, 256, 3)).astype(np.float32)
+    mean_of_dataset = np.zeros((image_size[0], image_size[1], 3)).astype(np.float32)
     size_of_dataset = 0
 
     if osp.exists(dataset_dir):
@@ -78,24 +100,29 @@ def split():
     os.mkdir(dataset_dir)
     # write how many classes
     classes = sorted(os.listdir(origin_dir))
-    with open(osp.join(dataset_dir, 'n_class.txt'), mode='w') as f:
+    with open(osp.join(root_dir, 'n_class.txt'), mode='w') as f:
         for class_name in classes:
             f.write(class_name + '\n')
     for class_id, class_name in enumerate(classes):
         file_names = os.listdir(osp.join(origin_dir, class_name))
         file_num = len(file_names)
-
         # copy train and test data
         # resize and augment data (multiple args.augment times)
-        for i, file_name in enumerate(file_names):
-            saved_file_name = class_name + file_name
+        image_num_per_class = min(args.number, file_num)
+        selected_images = random.sample(range(file_num), image_num_per_class)
+        for i, file_name in enumerate(np.array(file_names)[selected_images]):
+            if file_name.endswith('.png') is not True:
+                continue
+            saved_file_name = class_name + '_' + file_name
             img = Image_.open(osp.join(origin_dir, class_name, file_name))
-            img_resize = img.resize((256, 256))
+            img = img_jet(np.asarray(img))[:, :, [2, 1, 0]]  # bgr -> rgb
+            img = Image_.fromarray(img)
+            img_resize = img.resize((image_size[0], image_size[1]))
             mean_of_dataset += img_resize
             size_of_dataset += 1
-            if i < file_num * rate:  # save data for train
+            if i < image_num_per_class * rate:  # save data for train
                 saved_file_name = 'train_' + saved_file_name
-                for j in range(int(args.augment)):
+                for j in range(args.augment):
                     _ = osp.splitext(saved_file_name)
                     saved_file_name_augmented = _[0] + '_{0:03d}'.format(j) + _[1]
                     img_aug = Image_.fromarray(seq.augment_image(np.array(img_resize)))
